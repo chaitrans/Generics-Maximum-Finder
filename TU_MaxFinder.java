18b2d8e946fc1b28b4778e0e8baf80c43233fbc3
chaitra845 <chaitrachytra@gmail.com>
2021-06-20 13:30:13 +0530

    Added Float Method

--- TU_MaxFinder.java
+++ TU_MaxFinder.java
@@ -4,23 +4,23 @@ import org.junit.jupiter.api.Test;
 
 public class TU_MaxFinder {
     @Test
-    void whenIntegerShouldReturnMax1(){
-        Integer int1 = 100, int2 = 50, int3 = 10;
-        Integer max = MaxFinder.findMax(int1, int2, int3);
-        Assertions.assertEquals(100, max, "Validating max method");
+    void whenFloatShouldReturnMax1() {
+        Float num1 = 10.5f, num2 = 5.5f, num3 = 1.5f;
+        Float max = MaxFinder.findMax(num1, num2, num3);
+        Assertions.assertEquals(10.5f, max, "Validating max method");
     }
 
     @Test
-    void whenIntegerShouldReturnMax2() {
-        Integer int1 = 50, int2 = 100, int3 = 10;
-        Integer max = MaxFinder.findMax(int1, int2, int3);
-        Assertions.assertEquals(100, max, "Validating max method");
+    void whenFloatShouldReturnMax2() {
+        Float num1 = 1.5f, num2 = 11.5f, num3 = 5.5f;
+        Float max = MaxFinder.findMax(num1, num2, num3);
+        Assertions.assertEquals(11.5f, max, "Validating max method");
     }
 
     @Test
-    void whenIntegerShouldReturnMax3() {
-        Integer int1 = 10, int2 = 50, int3 = 100;
-        Integer max = MaxFinder.findMax(int1, int2, int3);
-        Assertions.assertEquals(100, max, "Validating max method");
+    void whenFloatShouldReturnMax3() {
+        Float num1 = 5.5f, num2 = 1.5f, num3 = 110.5f;
+        Float max = MaxFinder.findMax(num1, num2, num3);
+        Assertions.assertEquals(110.5f, max, "Validating max method");
     }
 }
