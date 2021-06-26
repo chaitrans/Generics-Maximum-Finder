b4c0a347c83446e04a4395d4b94307b4780c9f7b
chaitra845 <chaitrachytra@gmail.com>
2021-06-26 13:59:21 +0530

    Refactored all 3 to 1 generic method

---
+++ TU_MaximumFinder.java
@@ -0,0 +1,70 @@
+import com.chaitra.max.MaxFinder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class TU_MaximumFinder {
+
+    @Test
+    void whenIntegerShouldReturnMax1() {
+        Integer int1 = 6, int2 = 1, int3 = 4;
+        Integer max = MaxFinder.findMax(int1, int2, int3);
+        Assertions.assertEquals(6, max, "Validating max method");
+    }
+
+    @Test
+    void whenIntegerShouldReturnMax2() {
+        Integer int1 = 1, int2 = 6, int3 = 4;
+        Integer max = MaxFinder.findMax(int1, int2, int3);
+        Assertions.assertEquals(6, max, "Validating max method");
+    }
+
+    @Test
+    void whenIntegerShouldReturnMax3() {
+        Integer int1 = 1, int2 = 4, int3 = 6;
+        Integer max = MaxFinder.findMax(int1, int2, int3);
+        Assertions.assertEquals(6, max, "Validating max method");
+    }
+
+    @Test
+    void whenFloatShouldReturnMax1() {
+        Float num1 = 11.2f, num2 = 6.2f, num3 = 4.3f;
+        Float max = MaxFinder.findMax(num1, num2, num3);
+        Assertions.assertEquals(11.2f, max, "Validating max method");
+    }
+
+    @Test
+    void whenFloatShouldReturnMax2() {
+        Float num1 = 4.3f, num2 = 11.2f, num3 = 6.2f;
+        Float max = MaxFinder.findMax(num1, num2, num3);
+        Assertions.assertEquals(11.2f, max, "Validating max method");
+    }
+
+    @Test
+    void whenFloatShouldReturnMax3() {
+        Float num1 = 4.3f, num2 = 6.2f, num3 = 11.2f;
+        Float max = MaxFinder.findMax(num1, num2, num3);
+        Assertions.assertEquals(11.2f, max, "Validating max method");
+    }
+
+    @Test
+    void whenStringShouldReturnMax1() {
+        String str1="Peach", str2 = "Apple", str3 = "Banana";
+        String maxString = MaxFinder.findMax(str1, str2, str3);
+        Assertions.assertEquals("Peach", maxString, "Validating maxFinder function");
+    }
+
+    @Test
+    void whenStringShouldReturnMax2() {
+        String str1="Apple", str2 = "Peach", str3 = "Banana";
+        String maxString = MaxFinder.findMax(str1, str2, str3);
+        Assertions.assertEquals("Peach", maxString, "Validating maxFinder function");
+    }
+
+    @Test
+    void whenStringShouldReturnMax3() {
+        String str1="Banana", str2 = "Apple", str3 = "Peach";
+        String maxString = MaxFinder.findMax(str1, str2, str3);
+        Assertions.assertEquals("Peach", maxString, "Validating maxFinder function");
+    }
+}
---
+++ com/chaitra/max/MaxFinder.java
@@ -0,0 +1,15 @@
+package com.chaitra.max;
+
+public class MaxFinder {
+    public static <T extends Comparable> T findMax(T value1, T value2, T value3) {
+        T max = value1;
+        if (value2.compareTo(max) > 0) {
+            max = value2;
+        }
+        if (value3.compareTo(max) > 0) {
+            max = value3;
+        }
+        System.out.printf("Maximum of %s %s %s is %s \n", value1, value2, value3, max);
+        return max;
+    }
+}
